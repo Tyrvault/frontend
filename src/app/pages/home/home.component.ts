92eecaf639c2f0e52371b5247f575873f144f47e
Rodolfo Guimarães <133808003+Tyrvault@users.noreply.github.com>
2023-10-06 08:53:46 -0300

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,10 +11,10 @@ import { ApiService } from 'src/app/api.service';
 export class HomeComponent {
   
   eventos = [
-    { id: 1, local: 'Bodega', data: '10/10/2023', hora: '22:00', descricao: 'ingressos R$ 10,00' },
-    { id: 2, local: 'Bodega', data: '10/10/2023', hora: '22:00', descricao: 'ingressos R$ 10,00' },
-    { id: 3, local: 'Bodega', data: '10/10/2023', hora: '22:00', descricao: 'ingressos R$ 10,00' },
-    { id: 4, local: 'Bodega', data: '10/10/2023', hora: '22:00', descricao: 'ingressos R$ 10,00' },
+    { id: 1, local: 'Expopato', data: '08/10/2023', hora: '22:00', descricao: 'Ingressos R$ 10,00' },
+    { id: 2, local: 'Ginásio Patão', data: '14/10/2023', hora: '20:00', descricao: 'Ingressos R$ 15,00' },
+    { id: 3, local: 'Praça Presidente Vargas', data: '21/10/2023', hora: '22:00', descricao: 'Ingressos R$ 20,00' },
+    { id: 4, local: 'Sesc', data: '10/10/2023', hora: '22:00', descricao: 'Entrada Gratuita' },
   ]
   selected_evento = {id:0, local: ''};
 
